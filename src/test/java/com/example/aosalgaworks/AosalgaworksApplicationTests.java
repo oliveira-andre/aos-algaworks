0501385f9d46658902143d57a7a9a8d55f9a4cfe
Andre Oliveira <desmonplays@gmail.com>
2020-04-23 15:10:12 -0400

    (new feature) test folder

---
+++ src/test/java/com/example/aosalgaworks/AosalgaworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aosalgaworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AosalgaworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
